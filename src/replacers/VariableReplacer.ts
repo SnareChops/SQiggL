1a3de74539eb7a05a035a074aaab1f8df1a4b835
SnareChops <snare_chops@yahoo.com>
2015-06-18 13:24:32 -0400

    Adding VariableReplacer tests

--- src/replacers/VariableReplacer.ts
+++ src/replacers/VariableReplacer.ts
@@ -2,10 +2,15 @@
 import {IVariables} from '../IVariables';
 
 export default class VariableReplacer {
-	public static regex: RegExp = /{{\s*(\w*)\s*}}/g;
+	public static regex: RegExp = /([^{]|^){{(?!{)\s*(\w*)\s*}}(?!})/g;
 	
 	public static replace(text: string, variables: IVariables): string{
 		console.log(variables);
-		return text.replace(this.regex, (match, $1) => variables[$1]);
+		console.log(text.replace(this.regex, (match, $1, $2) => {
+			console.log($1);
+			console.log($2);
+			return $1+variables[$2]
+		}));
+		return text.replace(this.regex, (match, $1, $2) => $1+variables[$2]);
 	}
 }
