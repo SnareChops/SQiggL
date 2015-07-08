9a4d892459c0edfd96aebdba38037a3e931f0981
SnareChops <snare_chops@yahoo.com>
2015-07-08 12:40:38 -0400

    Fixing bug in extractModifiers logic

--- src/conditions/Condition.ts
+++ src/conditions/Condition.ts
@@ -6,15 +6,15 @@ export default class Condition {
         return klass.modifiers.map(x => `${x.identifiers.map(id => id.source).join('|')}`).join('|');
     }
     public extractModifiers(klass, mod1: string, mod2: string): any[]{
-        if(mod1 == null && mod2 == null) return [];
+        if(!mod1 && !mod2) return [];
         let array = [], count = 0;
-        if(mod1 != null) count++;
-        if(mod2 != null) count++;
+        if(mod1) count++;
+        if(mod2) count++;
         for(let mod of klass.modifiers){
             for(let identifier of mod.identifiers){
-                if(mod1 != null && identifier.test(mod1)) array[0] = mod;
-                if(mod2 != null && identifier.test(mod2)) {
-                    array[mod1 == null ? 0 : 1] = mod;
+                if(mod1 && identifier.test(mod1)) array[0] = mod;
+                if(mod2 && identifier.test(mod2)) {
+                    array[!mod1 ? 0 : 1] = mod;
                 }
                 if(array.length === count) return array;
             }
@@ -24,7 +24,7 @@ export default class Condition {
     public performModifiers(modifiers: IModifier[], result: boolean, variable: string, variables: IVariables, comparative: string): boolean{
         if(modifiers.length === 0) return result;
         let i;
-        for(i=modifiers.length - 1;i>-1;i--){
+        for(i=modifiers.length - 1;i>-1;--i){
             result = modifiers[i].perform(result, variable, variables, comparative);
         }
         return result;
